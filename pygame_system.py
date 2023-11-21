3978711ae45127bd2a0f2cbe27e548465cefad63
hinata <c0a2212903@edu.teu.ac.jp>
2023-11-21 14:37:22 +0900

    サイズの変更

--- pygame_system.py
+++ pygame_system.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     pg.display.set_caption("はじめてのPygame")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((900, 600))
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
 
